5f95c31c1d98ce49e93b993aea3ba6779d24861c
Dhruv Dubey <simplycode07@gmail.com>
2024-03-03 00:59:46 +0530

    Added structs for template and loading up font

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.0
 require github.com/spf13/cobra v1.8.0
 
 require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/image v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,42 +4,75 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
-	"os"
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
+	"os"
+	"path"
 
+	"github.com/golang/freetype/truetype"
 	"github.com/simplycode07/certificate-cli/cmd"
-	"github.com/simplycode07/certificate-cli/image"
+	"github.com/simplycode07/certificate-cli/generator"
 )
+
 
 func main() {
 	templatePath, blankCertificatePath, nameListPath := cmd.Execute()
-	fmt.Println(templatePath)
-	fmt.Println(blankCertificatePath)
-	fmt.Println(nameListPath)
+	var baseDir string = path.Dir(templatePath)
+	baseDir += "/"
+
+	fmt.Println("Base Directory:", baseDir)
+	fmt.Println("Template Path:", templatePath)
+	fmt.Println("Blank Certificate:", blankCertificatePath)
+	fmt.Println("Names of attendees:", nameListPath)
 
 	// read settings from template file
-	var template interface{}
+	var template generator.Template
 	dataByte, _ := os.ReadFile(templatePath)
-	json.Unmarshal(dataByte, &template)
-
-	// fmt.Println(template)
-	nameBytes, _ := os.Open(nameListPath)
-	names, err := csv.NewReader(nameBytes).ReadAll()
-	nameBytes.Close()
-
+	err := json.Unmarshal(dataByte, &template)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Println("NAMES: ")
-	for _, val := range names {
-		for _, name := range val{
-			fmt.Printf("%s, ", name)
-			image.GenerateImage(template, blankCertificatePath, name)
-		}
+	// read list of names from the csv file
+	nameBytes, err := os.Open(nameListPath)
+	// names, err := csv.NewReader(nameBytes).ReadAll()
+	_, err = csv.NewReader(nameBytes).ReadAll()
+	defer nameBytes.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	certificateByte, err := os.Open(blankCertificatePath)
+	defer certificateByte.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// read and parse font file
+	fontBytes, err := os.ReadFile(baseDir + template.Serial.Font)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	parsedFont, err := truetype.Parse(fontBytes)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+
+	generator.GenerateImage(template, certificateByte, parsedFont, "name", baseDir)
+
+	// fmt.Println("NAMES: ")
+	// for _, val := range names {
+	// 	for _, name := range val{
+	// 		// fmt.Printf("%s, ", name)
+	// 		generator.GenerateImage(template, certificateByte, blankCertificatePath, name)
+	// 	}
+	// }
 	fmt.Println()
 }
